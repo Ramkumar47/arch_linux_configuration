41e6a8798217430f2c46f3fe78768f4f9ee8dd5f
Ramkumar <ramkumar21194@gmail.com>
2021-02-27 11:15:40 +0530

    custom init scripts p5js

---
+++ scripts/src_p5js/template.js
@@ -0,0 +1,12 @@
+// defining width and height of canvas
+let width=400, height=400;
+
+function setup(){
+	// creating canvas
+	createCanvas(width,height);
+}
+
+function draw(){
+	// setting background color
+	background(0);
+}
